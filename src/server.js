c9baca33262a41d5ce1f01ea9a341ac287635783
krzysztofjerzyk <krzysztof.jerzyk@merito.pl>
2025-09-17 10:58:11 +0200

    save

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import express from "express";
 import { connectDb } from "./db/connectDb.js";
 import { getMarketData } from "./services/binance.service.js";
 import { klinesDiff } from "./utils/utils.js";
+import { sortBy } from "lodash-es";
 
 dotenv.config();
 
@@ -19,14 +20,28 @@ const interval = process.env.INTERVAL;
 const start = async () => {
   await connectDb();
 
+  //pobieranie historycznych swiec z marketu dla danego symbolu i intervale
   const symbolKlines = await getMarketData(symbol, interval);
 
-  const calcData = symbolKlines.map((kline) => {
-    return klinesDiff(
-      parseFloat(kline.openPrice),
-      parseFloat(kline.closePrice),
-    );
-  });
+  // wykaz wzrostu/spadku ceny w danej swiecy
+  const symbolKlinesWithDelta = sortBy(
+    symbolKlines.map((kline) => {
+      const delta = klinesDiff(
+        parseFloat(kline.openPrice),
+        parseFloat(kline.closePrice),
+      );
+
+      return {
+        ...kline,
+        delta,
+      };
+    }),
+    "openTime",
+  );
+
+  symbolKlinesWithDelta.map((kline) => {});
+
+  console.log(symbolKlinesWithDelta);
 
   app.listen(port, () => {
     console.info(`Listening on port ${port}`);
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,2 +1,2 @@
-// obliczanie procentowej zmiany cen miedzy otwarcie a zamknieciem swiecy
-export const klinesDiff = (open, close) => ((close - open) / close) * 100;
+// obliczanie zmiany cen miedzy otwarcie a zamknieciem swiecy
+export const klinesDiff = (open, close) => (close - open) / open;
