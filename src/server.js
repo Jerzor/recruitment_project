e3b7b502ce3d2ab1c05c37081290a4834ad33135
krzysztofjerzyk <krzysztof.jerzyk@merito.pl>
2025-09-17 12:48:42 +0200

    save

--- src/server.js
+++ src/server.js
@@ -17,6 +17,75 @@ const port = process.env.PORT || 3000;
 const symbol = process.env.SYMBOL;
 const interval = process.env.INTERVAL;
 
+const reportPreview = (
+  symbolKlinesWithDelta,
+  deltaMax,
+  deltaMin,
+  cumulativePercentage,
+) => {
+  console.log(
+    `Świece dla interwału: ${interval}, dla symbolu: ${symbol}: `,
+    symbolKlinesWithDelta,
+  );
+
+  console.log(`Maksymalne odchylenie delta = ${deltaMax}`);
+  console.log(`Minimalne odchylenie delta = ${deltaMin}`);
+
+  console.log(
+    "Skumulowany zwrot okresu w ujeciu prostym:",
+    cumulativePercentage,
+  );
+};
+
+const countDeltas = (symbolKlinesWithDelta) => {
+  const deltaMax = maxBy(symbolKlinesWithDelta, "delta").delta.toFixed(4);
+  const deltaMin = minBy(symbolKlinesWithDelta, "delta").delta.toFixed(4);
+
+  return { deltaMax, deltaMin };
+};
+
+const Colors = {
+  Red: "red",
+  Green: "green",
+  Neutral: "neutral",
+};
+
+const getKlineColor = (kline) => {
+  console.log(kline);
+  // kolor mówiacy o tym, czy swieca zamkneła sie ze wzrostem, spadkiem czy bez zmain
+  // mozna tez po delcie sprawdzać
+
+  const openPrice = parseFloat(kline.openPrice);
+  const closePrice = parseFloat(kline.closePrice);
+
+  let color;
+
+  if (closePrice === openPrice) color = Colors.Neutral;
+  else if (closePrice > openPrice) color = Colors.Green;
+  else if (closePrice < openPrice) color = Colors.Red;
+
+  console.log(color);
+
+  return color;
+};
+
+const getFormatteData = (symbolKlines) => {
+  return symbolKlines.map((kline) => {
+    const delta = klinesDiff(
+      parseFloat(kline.openPrice),
+      parseFloat(kline.closePrice),
+    );
+
+    const klineColor = getKlineColor(kline);
+
+    return {
+      ...kline,
+      delta,
+      klineColor,
+    };
+  });
+};
+
 const start = async () => {
   // nie ma potrzeby łączenia sie z DB
   // await connectDb();
@@ -24,31 +93,10 @@ const start = async () => {
   //pobieranie historycznych swiec z marketu dla danego symbolu i intervale
   const symbolKlines = await getMarketData(symbol, interval);
 
+  const formattedData = getFormatteData(symbolKlines);
+
   // wykaz wzrostu/spadku ceny w danej swiecy
-  const symbolKlinesWithDelta = sortBy(
-    symbolKlines.map((kline) => {
-      const delta = klinesDiff(
-        parseFloat(kline.openPrice),
-        parseFloat(kline.closePrice),
-      );
-
-      // kolor mówiacy o tym, czy swieca zamkneła sie ze wzrostem, spadkiem czy bez zmain
-      // mozna tez po delcie sprawdzać
-      const klineColor =
-        kline.closePrice === kline.openPrice
-          ? "neutral"
-          : kline.closePrice > kline.openPrice
-            ? "green"
-            : "red";
-
-      return {
-        ...kline,
-        delta,
-        klineColor,
-      };
-    }),
-    "openTime",
-  );
+  const symbolKlinesWithDelta = sortBy(formattedData, "openTime");
 
   const sortedDeltas = symbolKlinesWithDelta.map((kline) => {
     return kline.delta;
@@ -65,19 +113,12 @@ const start = async () => {
   const cumulative = (accumulator - 1) * 100;
   const cumulativePercentage = `${cumulative.toFixed(4)}%`;
 
-  console.log(
-    `Świece dla interwału: ${interval}, dla symbolu: ${symbol}: `,
+  const { deltaMax, deltaMin } = countDeltas(symbolKlinesWithDelta);
+
+  reportPreview(
     symbolKlinesWithDelta,
-  );
-
-  const deltaMax = maxBy(symbolKlinesWithDelta, "delta").delta.toFixed(4);
-  const deltaMin = minBy(symbolKlinesWithDelta, "delta").delta.toFixed(4);
-
-  console.log(`Maksymalne odchylenie delta = ${deltaMax}`);
-  console.log(`Minimalne odchylenie delta = ${deltaMin}`);
-
-  console.log(
-    "Skumulowany zwrot okresu w ujeciu prostym:",
+    deltaMax,
+    deltaMin,
     cumulativePercentage,
   );
 
